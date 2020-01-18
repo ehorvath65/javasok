22fa4d1b745136539b387ca1581980459014dc20
Edit Horváth <edit.horvath@gmail.com>
2020-01-18 22:19:59 +0100

    regular expressions

--- src/main/java/javasok/Javasok.java
+++ src/main/java/javasok/Javasok.java
@@ -3,11 +3,16 @@ package javasok;
 public class Javasok {
 	
 	public static void main(String args[]) {
-		Splitter splitter = new Splitter();
-		splitter.egy();
-		splitter.ketto();
-		splitter.harom();
-		splitter.negy();
+//		Splitter splitter = new Splitter();
+//		splitter.egy();
+//		splitter.ketto();
+//		splitter.harom();
+//		splitter.negy();
+		Regular regular = new Regular();
+		regular.csere();
+		regular.csere2();
+		regular.csere3();
+		regular.csere4();
 	}
 	
 }
---
+++ src/main/java/javasok/Regular.java
@@ -0,0 +1,30 @@
+package javasok;
+
+public class Regular {
+
+	String fejlec = "Termék kód  ; Árlista   ; Egységár ;   Alapár (Nettó)  ;   Árrés % ;  Kedvezmény %";
+
+	void csere() {
+		String pipeCsere = fejlec.replace(";", "|");
+		System.out.println(pipeCsere);
+	}
+
+	void csere2() {
+		String pipeCsere = fejlec.replaceAll(" *; *", "|");
+		System.out.println("2. " + pipeCsere);
+	}
+	
+	void csere3() {
+		String[] stringTomb = fejlec.split(" *; *");
+
+		for (String item : stringTomb) {
+			System.out.println(item);
+		}
+	}
+	
+	void csere4() {
+		String pipeCsere = fejlec.replaceAll(" *; *", "|").replaceFirst("(\\|[^\\|]+){2}$", "");
+		System.out.println("4. " + pipeCsere);
+	}
+
+}
